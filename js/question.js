990e96ff8b7e9fa1b31d874fa86807a7411b8613
Marcio Murua <marciomurua1995@gmail.com>
2022-10-14 19:26:03 -0300

    Slider
    Avances en el slider de las preguntas

--- js/question.js
+++ js/question.js
@@ -1,4 +1,22 @@
 (function(){
-    const titleQuestions = [...document.querySelectorAll('question__slider--all')]
+    const titleQuestions = [...document.querySelectorAll('.question__slider--all')];
+    console.log(titleQuestions);
+    titleQuestions.forEach(question =>{
+        question.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement
+
+            addPadding.classList.toggle('question')
+
+            question.children[0].classList.toggle('questions__img--rotate');
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.getElementsByClassName.height = `${height}px`;
+        })
+    })
 
 })();
