6fac9165432d6cc742d95eb4e38c0a3707fc6cc5
Marcio Murua <marciomurua1995@gmail.com>
2022-12-30 01:08:10 -0300

    continuacion de la pagina footer

--- js/questions.js
+++ js/questions.js
@@ -1,21 +1,20 @@
 (function(){
-    const titleQuestions = [...document.querySelectorAll('.question__slider--all')];
+    const titleQuestions = [...document.querySelectorAll('.questions__title')];
     console.log(titleQuestions);
     titleQuestions.forEach(question =>{
         question.addEventListener('click', ()=>{
             let height = 0;
             let answer = question.nextElementSibling;
-            let addPadding = question.parentElement.parentElement
+            let addPadding = question.parentElement.parentElement;
 
-            addPadding.classList.toggle('question')
-
-            question.children[0].classList.toggle('questions__img--rotate');
+            addPadding.classList.toggle('questions__padding--add');
+            question.children[0].classList.toggle('questions__arrow--rotate');
 
             if(answer.clientHeight === 0){
                 height = answer.scrollHeight;
             }
 
-            answer.getElementsByClassName.height = `${height}px`;
+            answer.style.height = `${height}px`;
         })
     })
 
